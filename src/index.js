789508c5c4b5843a23809c99f8312f9db1bcb3e3
eaasy-dev <ogodono@eaasy.email>
2024-11-08 14:06:06 -0800

    Object Branch commit 11-08-24

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { companiesData } from "./companies-data";
+import {extraRecord } from "./extra-record";
+
+console.log ("companiesData", companiesData);
+console.log("Extra" , extraRecord);
 
 const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
 const manipulatedData = [];
 //TODO: add the manipulated data array to the 'after' div.
-const after = document.getElementById.ElemementByID("after");
+//const after = document.getElementById.ElemementByID("after");
+const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData,null,2);
+console.log("hellow world from object branch?");
